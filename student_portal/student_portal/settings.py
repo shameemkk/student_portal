2489b4b492a6f102c4ae847f57a4cea228e18619
shameem <shameemkk4@gmail.com>
2025-08-14 00:55:07 +0530

    Set DEBUG to False and allow all hosts in settings.py for production readiness

--- student_portal/student_portal/settings.py
+++ student_portal/student_portal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*oh&!vy1se86+mc6j7ltj$&z#g3*4druvz0!t3no=@le$=ni)f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
